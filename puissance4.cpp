281431f0c703cc75dd93442e92a7c37ccd307018
Zouaf <122008142+Zouaf@users.noreply.github.com>
2023-01-05 16:24:50 +0100

    Add files via upload

--- puissance4.cpp
+++ puissance4.cpp
@@ -1,5 +1,10 @@
 #include "puissance4.h"
 #include "./ui_puissance4.h"
+#include <QWidget>
+#include <QLabel>
+#include <QFont>
+#include <QHBoxLayout>
+#include <QRect>
 
 Puissance4::Puissance4(QWidget *parent)
     : QMainWindow(parent)
@@ -16,9 +21,33 @@ Puissance4::~Puissance4()
 
 void Puissance4::on_playButton_clicked()
 {
-    ui->PlayPB->deleteLater();
-    ui->QuitPB->deleteLater();
-    ui->SettingsPB->deleteLater();
-    ui->ResultPB->deleteLater();
+    ui->PlayPB->deleteLater();                          // supprime le bouton
+    ui->QuitPB->deleteLater();                          // supprime le bouton
+    ui->SettingsPB->deleteLater();                      // supprime le bouton
+    ui->ResultPB->deleteLater();                        // supprime le bouton
+
+
+
+    QLabel *teamLB = new QLabel();                      //creation d'un label
+    QLabel *team2LB = new QLabel();
+    QGridLayout *layout = new QGridLayout();            //creation d'un layout
+    teamLB->setText("Equipe 1");                        //Ecrit "Equipe 1" dans le label
+    teamLB->setStyleSheet("color:red");                 // Met en rouge le texte du label
+    QFont newfont ( "Equipe 1", 18, QFont::Bold,false); // parametre la police ainsi que la taille du Label
+    team2LB->setText("Equipe 2");                        //Ecrit "Equipe 1" dans le label
+    team2LB->setStyleSheet("color:Blue");                 // Met en rouge le texte du label
+    QFont newfont2 ( "Equipe 2", 18, QFont::Bold,false); // parametre la police ainsi que la taille du Label
+
+    teamLB->setFont(newfont);                           //parametre la police ainsi que la taille du Label
+    team2LB->setFont(newfont2);
+
+    layout->addWidget(teamLB,0,1);                          // met le label dans un layout
+    layout->addWidget(team2LB,0,2);
+    layout->setContentsMargins(125,0,0,400);            //change le layout de position
+    ui->centralwidget->setLayout(layout);               // met le layout dans l'ui
+
+
+
+
 }
 
