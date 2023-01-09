c98f6bb64f9a31524e0b30792f1e5d6eb4697389
Zouaf <122008142+Zouaf@users.noreply.github.com>
2023-01-09 16:55:03 +0100

    Add files via upload

--- puissance4.cpp
+++ puissance4.cpp
@@ -5,6 +5,8 @@
 #include <QFont>
 #include <QHBoxLayout>
 #include <QRect>
+#include <QComboBox>
+#include <QFontComboBox>
 
 Puissance4::Puissance4(QWidget *parent)
     : QMainWindow(parent)
@@ -21,16 +23,41 @@ Puissance4::~Puissance4()
 
 void Puissance4::on_playButton_clicked()
 {
-    ui->PlayPB->deleteLater();                          // supprime le bouton
-    ui->QuitPB->deleteLater();                          // supprime le bouton
-    ui->SettingsPB->deleteLater();                      // supprime le bouton
-    ui->ResultPB->deleteLater();                        // supprime le bouton
+    ui->PlayPB->hide();                          // supprime le bouton
+    ui->QuitPB->hide();                          // supprime le bouton
+    ui->SettingsPB->hide();                      // supprime le bouton
+    ui->ResultPB->hide();                        // supprime le bouton
+
+    createLayout();
+
+    connect(confirmPlayPB, SIGNAL(clicked()), this, SLOT(play_button()));
+    connect(quitConfirmPB, SIGNAL(clicked()), this, SLOT(quit_button()));
+
+}
+
+void Puissance4::createCombox()
+{
+    QComboBox *combo = new QComboBox();
+    combo->addItem("/");
+    combo->addItem("matéo");
+    combo->addItem("Anthony");
+    combo->addItem("Le pompier");
+    combo->addItem("thibault");
+    combo->setMaximumWidth(100);
+
+    layout->addWidget(combo,i,j);
+
+}
+
+void Puissance4::createLayout()
+{
+    teamLB = new QLabel();
+    team2LB = new QLabel();
+    layout = new QGridLayout();
+    confirmPlayPB = new QPushButton(ui->centralwidget);
+    quitConfirmPB = new QPushButton(ui->centralwidget);
 
 
-
-    QLabel *teamLB = new QLabel();                      //creation d'un label
-    QLabel *team2LB = new QLabel();
-    QGridLayout *layout = new QGridLayout();            //creation d'un layout
     teamLB->setText("Equipe 1");                        //Ecrit "Equipe 1" dans le label
     teamLB->setStyleSheet("color:red");                 // Met en rouge le texte du label
     QFont newfont ( "Equipe 1", 18, QFont::Bold,false); // parametre la police ainsi que la taille du Label
@@ -43,11 +70,65 @@ void Puissance4::on_playButton_clicked()
 
     layout->addWidget(teamLB,0,1);                          // met le label dans un layout
     layout->addWidget(team2LB,0,2);
-    layout->setContentsMargins(125,0,0,400);            //change le layout de position
+    layout->setContentsMargins(0,0,0,300);            //change le layout de position
+
+    for(j=1;j<3;j++)
+    {
+        for(i=1; i<5; i++)
+        {
+            createCombox();
+        }
+    }
+    confirmPlayPB->setText("Jouer");
+    confirmPlayPB->setGeometry(500,520,100,55);
+    confirmPlayPB->show();
+    quitConfirmPB->setText("Quitter");
+    quitConfirmPB->setGeometry(20,520,100,55);
+    quitConfirmPB->show();
     ui->centralwidget->setLayout(layout);               // met le layout dans l'ui
 
+}
 
+
+void clearLayout(QLayout *layout) {
+    if (layout == NULL)
+        return;
+    QLayoutItem *item;
+    while((item = layout->takeAt(0))) {
+        if (item->layout()) {
+            clearLayout(item->layout());
+            delete item->layout();
+        }
+        if (item->widget()) {
+           delete item->widget();
+        }
+        delete item;
+    }
+}
+
+void Puissance4::play_button()
+{
+    qDebug() << "e";
+    quitConfirmPB->hide();
+    confirmPlayPB->hide();
+    clearLayout(layout);
+    layout->deleteLater();
 
 
 }
+
+void Puissance4::quit_button()
+{
+    qDebug() << "e";
+    quitConfirmPB->deleteLater();
+    confirmPlayPB->deleteLater();
+    clearLayout(layout);
+    layout->deleteLater();
+    ui->PlayPB->show();
+    ui->SettingsPB->show();
+    ui->ResultPB->show();
+    ui->QuitPB->show();
+}
+
+
 
--- puissance4.h
+++ puissance4.h
@@ -2,6 +2,14 @@
 #define PUISSANCE4_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QLabel>
+#include <QFont>
+#include <QHBoxLayout>
+#include <QRect>
+#include <QComboBox>
+#include <QFontComboBox>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Puissance4; }
@@ -15,8 +23,23 @@ public:
     Puissance4(QWidget *parent = nullptr);
     ~Puissance4();
     void on_playButton_clicked();
+    void createCombox();
+    void createLayout();
+
+
+public slots:
+    void play_button();
+    void quit_button();
+
 
 private:
     Ui::Puissance4 *ui;
+    QLabel *teamLB;                     //creation d'un label
+    QLabel *team2LB;
+    QGridLayout *layout;            //creation d'un layout
+    QPushButton *confirmPlayPB;
+    QPushButton *quitConfirmPB;
+    int i;
+    int j;
 };
 #endif // PUISSANCE4_H
