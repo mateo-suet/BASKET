8e5fa205b07f9070197576bd6d646e2a8e7a4710
mateo-suet <122024509+mateo-suet@users.noreply.github.com>
2023-01-09 17:06:13 +0100

    Add files via upload

--- puissance4.cpp
+++ puissance4.cpp
@@ -1,8 +1,6 @@
 #include "puissance4.h"
 #include "ui_puissance4.h"
-#include <QRadioButton>
-#include <QHBoxLayout>
-#include <QGridLayout>
+
 
 puissance4::puissance4(QWidget *parent)
     : QMainWindow(parent)
@@ -10,7 +8,7 @@ puissance4::puissance4(QWidget *parent)
 {
     ui->setupUi(this);
 
-    connect(ui->SettingsPB, &QPushButton::clicked, this, &puissance4::on_sett_clicked);
+    connect(ui->SettingsPB, &QPushButton::clicked, this, &puissance4::on_sett_clicked);    
 }
 
 puissance4::~puissance4()
@@ -21,23 +19,85 @@ puissance4::~puissance4()
 
 void puissance4::on_sett_clicked()
 {
-    ui->PlayPB->deleteLater();
-    ui->QuitPB->deleteLater();
-    ui->SettingsPB->deleteLater();
-    ui->ResultPB->deleteLater();
+    ui->PlayPB->hide();  //supprime tout les premier bouton
+    ui->QuitPB->hide();
+    ui->SettingsPB->hide();
+    ui->ResultPB->hide();
 
-    QRadioButton *nb4 = new QRadioButton("5",this);  // Affiche les boutons de 5 a 7
-    QRadioButton *nb6 = new QRadioButton("7",this);
+    creation_des_widget();
 
-    QGridLayout *lay = new QGridLayout();
+    connect(Bouton_ok, SIGNAL(clicked()), this, SLOT(le_bouton_ok()));
+    connect(Bouton_quitter, SIGNAL(clicked()), this, SLOT(le_bouton_quitter()));
+    connect(Bouton_default, SIGNAL(clicked()), this, SLOT(le_bouton_default()));
+}
+
+void clearLayout(QLayout *layout){
+    if (layout == NULL)
+        return;
+    QLayoutItem *item;
+    while ((item = layout->takeAt(0))) {
+        if (  (item->layout())){
+              clearLayout(item->layout());
+            delete item->layout();
+        }
+        if (item->widget()) {
+            delete item->widget();
+        }
+        delete item;
+    }
+}
+
+void puissance4::le_bouton_ok()
+{
+
+}
+
+void puissance4::le_bouton_quitter()
+{
+    clearLayout(lay);
+    lay->deleteLater();
+    ui->PlayPB->show();  //supprime tout les premier bouton
+    ui->QuitPB->show();
+    ui->SettingsPB->show();
+    ui->ResultPB->show();
+}
+
+void puissance4::le_bouton_default()
+{
+    nb5->setChecked(true);
+    LeTemps->setTime(QTime(0,5,0,0));
+}
+
+void puissance4::creation_des_widget()
+{
+    nb5 = new QRadioButton("5",this);  // Créer les boutons de 5 et 7
+    nb7 = new QRadioButton("7",this);
+    Bouton_ok = new QPushButton("OK"); //Créer les bouton ok quitter et default
+    Bouton_quitter = new QPushButton("Quitter");
+    Bouton_default = new QPushButton("Default");
+    titre = new QLabel(); //Créer les titres
+    titre1 = new QLabel();
+    LeTemps = new QTimeEdit(); //Créer la zone de temps
+
+    titre->setText("Nombre de Panier");
+    titre->setStyleSheet("font-size: 20px");
+
+    titre1->setText("Le Temps");
+    titre1->setStyleSheet("font-size: 20px");
+
+    lay = new QGridLayout();
     ui->LeTout->setLayout(lay);
 
-    lay->addWidget(nb4);
-    lay->addWidget(nb6,0,1);
+
+    lay->addWidget(titre);  //Place tout les widget dans le Layout
+    lay->addWidget(nb5,1,0);
+    lay->addWidget(nb7,1,1);
+    lay->addWidget(titre1,2,0);
+    lay->addWidget(LeTemps,3,0);
+    lay->addWidget(Bouton_ok,4,0);
+    lay->addWidget(Bouton_quitter,4,1);
+    lay->addWidget(Bouton_default,4,2);
 
     lay->setSizeConstraint(QLayout::SetFixedSize);
-    lay->setContentsMargins(350,100,0,0);
-
-
-
+    lay->setContentsMargins(10,10,0,0);
 }
--- puissance4.h
+++ puissance4.h
@@ -1,8 +1,15 @@
 #ifndef PUISSANCE4_H
 #define PUISSANCE4_H
-
+#include <QRadioButton>
 #include <QMainWindow>
 #include <QWidget>
+#include <QTimeEdit>
+#include <QObject>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QDebug>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class puissance4; }
@@ -20,9 +27,28 @@ public:
 
     void on_sett_clicked();
 
+    void creation_des_widget();
+
+public slots:
+    void le_bouton_ok();
+
+    void le_bouton_quitter();
+
+    void le_bouton_default();
 
 private:
     Ui::puissance4 *ui;
+
+    QRadioButton *nb5;
+    QRadioButton *nb7;
+
+    QTimeEdit *LeTemps;
+    QPushButton *Bouton_ok;
+    QPushButton *Bouton_quitter;
+    QPushButton *Bouton_default;
+    QLabel *titre;
+    QLabel *titre1;
+    QGridLayout *lay;
 
 };
 #endif // PUISSANCE4_H
