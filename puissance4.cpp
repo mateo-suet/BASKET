67763707935762dc8f1a4a57caa570059b018b6a
mateo-suet <122024509+mateo-suet@users.noreply.github.com>
2023-01-12 15:46:55 +0100

    Add files via upload

--- puissance4.cpp
+++ puissance4.cpp
@@ -19,16 +19,17 @@ puissance4::~puissance4()
 
 void puissance4::on_sett_clicked()
 {
-    ui->PlayPB->hide();  //supprime tout les premier bouton
+    ui->PlayPB->hide();  //cache tout les premier boutons
     ui->QuitPB->hide();
     ui->SettingsPB->hide();
     ui->ResultPB->hide();
 
-    creation_des_widget();
+    la_page_mdp();
 
-    connect(Bouton_ok, SIGNAL(clicked()), this, SLOT(le_bouton_ok()));
-    connect(Bouton_quitter, SIGNAL(clicked()), this, SLOT(le_bouton_quitter()));
-    connect(Bouton_default, SIGNAL(clicked()), this, SLOT(le_bouton_default()));
+
+//    connect(Bouton_ok, SIGNAL(clicked()), this, SLOT(le_bouton_ok()));
+//    connect(Bouton_quitter, SIGNAL(clicked()), this, SLOT(le_bouton_quitter()));
+//    connect(Bouton_default, SIGNAL(clicked()), this, SLOT(le_bouton_default()));
 }
 
 void clearLayout(QLayout *layout){
@@ -56,7 +57,7 @@ void puissance4::le_bouton_quitter()
 {
     clearLayout(lay);
     lay->deleteLater();
-    ui->PlayPB->show();  //supprime tout les premier bouton
+    ui->PlayPB->show();  //re affiche tout les premier boutons
     ui->QuitPB->show();
     ui->SettingsPB->show();
     ui->ResultPB->show();
@@ -64,26 +65,42 @@ void puissance4::le_bouton_quitter()
 
 void puissance4::le_bouton_default()
 {
+
     nb5->setChecked(true);
+    bson->setChecked(true);
     LeTemps->setTime(QTime(0,5,0,0));
+
 }
 
 void puissance4::creation_des_widget()
 {
+
     nb5 = new QRadioButton("5",this);  // Créer les boutons de 5 et 7
     nb7 = new QRadioButton("7",this);
+
+    bson = new QRadioButton("ON", this);
+    bsoff = new QRadioButton("OFF", this);
+
     Bouton_ok = new QPushButton("OK"); //Créer les bouton ok quitter et default
     Bouton_quitter = new QPushButton("Quitter");
     Bouton_default = new QPushButton("Default");
+
     titre = new QLabel(); //Créer les titres
     titre1 = new QLabel();
+    titreBS = new QLabel();
+
     LeTemps = new QTimeEdit(); //Créer la zone de temps
+
+    Groupe = new QButtonGroup(this);
 
     titre->setText("Nombre de Panier");
     titre->setStyleSheet("font-size: 20px");
 
     titre1->setText("Le Temps");
     titre1->setStyleSheet("font-size: 20px");
+
+    titreBS->setText("Son");
+    titreBS->setStyleSheet("font-size: 20px");
 
     lay = new QGridLayout();
     ui->LeTout->setLayout(lay);
@@ -92,12 +109,59 @@ void puissance4::creation_des_widget()
     lay->addWidget(titre);  //Place tout les widget dans le Layout
     lay->addWidget(nb5,1,0);
     lay->addWidget(nb7,1,1);
+
     lay->addWidget(titre1,2,0);
     lay->addWidget(LeTemps,3,0);
-    lay->addWidget(Bouton_ok,4,0);
-    lay->addWidget(Bouton_quitter,4,1);
-    lay->addWidget(Bouton_default,4,2);
+    lay->addWidget(titreBS,4,0);
 
-    lay->setSizeConstraint(QLayout::SetFixedSize);
-    lay->setContentsMargins(10,10,0,0);
+    lay->addWidget(bson,5,0);
+    lay->addWidget(bsoff,5,1);
+
+    lay->addWidget(Bouton_ok,6,0);
+    lay->addWidget(Bouton_quitter,6,1);
+    lay->addWidget(Bouton_default,6,2);
+
+    Groupe->addButton(nb5);
+    Groupe->addButton(nb7);
+
+    lay->setContentsMargins(250,10,250,250);
+}
+
+void puissance4::la_page_mdp()
+{
+    lay = new QGridLayout();
+
+    ligne_mdp = new QLineEdit();
+
+    lemdp = new QString("moi");
+
+    titre_mdp = new QLabel();
+    titre_mdp->setText("Mot de Passe");
+    titre_mdp->setStyleSheet("font-size: 25px");
+
+    ligne_mdp->setEchoMode(QLineEdit::Password);
+
+    c = new QString(ligne_mdp->selectedText());
+
+    Bouton_valider_mdp = new QPushButton("Valider");
+
+    ui->LeTout->setLayout(lay);
+
+    lay->addWidget(titre_mdp,0,0);
+    lay->addWidget(ligne_mdp,1,0);
+    lay->addWidget(Bouton_valider_mdp,2,0);
+    lay->setContentsMargins(300,100,250,300);
+
+    connect(Bouton_valider_mdp, SIGNAL(clicked()), this, SLOT(le_bouton_valider()));
+
+}
+
+void puissance4::le_bouton_valider()
+{
+    qDebug() << "on est dans le valider";
+    if(c == lemdp)
+    {
+        qDebug() << "on est dans le test";
+        creation_des_widget();
+    }
 }
--- puissance4.h
+++ puissance4.h
@@ -1,5 +1,6 @@
 #ifndef PUISSANCE4_H
 #define PUISSANCE4_H
+
 #include <QRadioButton>
 #include <QMainWindow>
 #include <QWidget>
@@ -10,6 +11,12 @@
 #include <QGridLayout>
 #include <QDebug>
 #include <QLabel>
+#include <QButtonGroup>
+#include <QString>
+#include <QLineEdit>
+//#include <QSqlQuery>
+//#include <QSqlDatabase>
+//#include <QSqlQuery>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class puissance4; }
@@ -29,6 +36,8 @@ public:
 
     void creation_des_widget();
 
+    void la_page_mdp();
+
 public slots:
     void le_bouton_ok();
 
@@ -36,19 +45,38 @@ public slots:
 
     void le_bouton_default();
 
+    void le_bouton_valider();
+
 private:
     Ui::puissance4 *ui;
 
     QRadioButton *nb5;
     QRadioButton *nb7;
+    QRadioButton *bson;
+    QRadioButton *bsoff;
 
     QTimeEdit *LeTemps;
+
     QPushButton *Bouton_ok;
     QPushButton *Bouton_quitter;
     QPushButton *Bouton_default;
+    QPushButton *Bouton_valider_mdp;
+
     QLabel *titre;
     QLabel *titre1;
+    QLabel *titreBS;
+    QLabel *titre_mdp;
+
     QGridLayout *lay;
+
+    QButtonGroup *Groupe;
+
+    QWidget *fenetre_mdp;
+
+    QLineEdit *ligne_mdp;
+
+    QString *lemdp;
+    QString *c;
 
 };
 #endif // PUISSANCE4_H
